91632209a992645d5bb389bddb9c73d497563408
Link4real <simon@die-duerrs.de>
2021-09-22 14:41:41 +0200

    y

---
+++ src/main/java/com/link/block/entity/Main.java
@@ -0,0 +1,18 @@
+package com.link.block.entity;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+
+
+
+
+
+
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
